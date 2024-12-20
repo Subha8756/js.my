ea032a9058568b51bd5e6cfb1d7cbb583ac823eb
Subha8756 <callsmastersubha@gmail.com>
2024-12-20 09:06:27 +0000

    test

--- my-js/new.js
+++ my-js/new.js
@@ -1,1 +1,7 @@
 console.log("subha")
+
+let name = "subha"
+let repoCount = 50
+// console.log(name + repoCount + "value")
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
